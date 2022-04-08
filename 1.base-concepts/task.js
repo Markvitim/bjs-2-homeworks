20604b8513d7b4b31b771d0de27c4560b1ad1f23
Markvitim <markvitim@gmail.com>
2022-04-08 17:26:10 +0300

    change task

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,18 @@
+"use strict";
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
+  let arr = [];
+  let d = b * b - (4 * a * c);
+
+  if (d > 0) {
+    let x = (-b + Math.sqrt(d)) / (2 * a);
+    let y = (-b - Math.sqrt(d)) / (2 * a);
+    arr.push(x, y);
+  } else if (d == 0) {
+    let z = -b / (2 * a);
+    arr.push(z);
+  } else {
+    arr = [];
+  }
   return arr; // array
 }
 
