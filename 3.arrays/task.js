25672dc010f2b6d7bd9adb299a9898d62997cbf1
Markvitim <markvitim@gmail.com>
2022-05-17 19:47:20 +0300

    arrays task

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,14 @@
-function compareArrays(arr1, arr2) {
-  let result;
 
-  // Ваш код
-
-  return result; // boolean
+let f = function compareArrays(arr1, arr2) {
+  let result = arr1.length === arr2.length && arr1.every((item, idx) => item === arr2[idx]);
+  return result;
 }
 
-function advancedFilter(arr) {
-  let resultArr;
 
-  // Ваш код
+function advancedFilter(arr) {
+  let resultArr = arr.filter(item => item > 0)
+    .filter(item => item % 3 === 0)
+    .map(item => item * 10);
 
   return resultArr; // array
 }
