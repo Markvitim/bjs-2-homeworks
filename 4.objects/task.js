ca720e7126df2b5b149ea4ec6848fe947aa141f9
Markvitim <markvitim@gmail.com>
2022-06-08 18:26:41 +0300

    objects

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,49 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
 
+const student1 = new Student("Anna", "female", 19);
+const student2 = new Student("Boris", "male", 21);
+const student3 = new Student("Galina", "female", 25);
+
+
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
+}
+
+Student.prototype.addMark = function (mark) {
+  if (this.marks === undefined) {
+    this.marks = [mark]
+  }
+  // добавить первую оценку 
+  else {
+    this.marks.push(mark)
+    // добавить вторую и последующие оценки в массив
+  }
+}
+Student.prototype.addMarks = function (...mark) {
+  if (this.marks === undefined) {
+    this.marks = [...mark]
+  } else {
+    this.marks.push(...mark)
+  }
+}
+
+Student.prototype.getAverage = function () {
+  const initialValue = 0;
+  let sumWithInitial = this.marks.reduce(
+    (previousValue, currentValue) => previousValue + currentValue,
+    initialValue
+  );
+  return sumWithInitial / this.marks.length;
+}
+
+Student.prototype.exclude = function (reason) {
+  delete this.subject;
+  delete this.mark;
+  this.excluded = reason;
 }
 
 // ваш код для остальных методов
